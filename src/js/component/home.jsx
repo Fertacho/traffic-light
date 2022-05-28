178e7fc95cbaca8051d245acbb2c61bcb1611b30
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-28 01:32:57 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 //create your first component
 const TrafficLight = () => {
 	return (
-		<div>
+		<div className="container">
 			<div className="lights">
 				<div className="light">a</div>
 				<div className="light">a</div>
