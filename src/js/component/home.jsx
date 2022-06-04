253e14154611ead285a9fe66c1a93f7af22a257a
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-04 00:26:43 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 //create your first component
 const TrafficLight = () => {
-	return (
+	//github.com/FabiofourGeeksacademy/ReactList
+	https: return (
 		<div className="container">
 			<div className="lights">
-				<div className="light">a</div>
-				<div className="light">a</div>
-				<div className="light">a</div>
+				<div className="light red glow"></div>
+				<div className="light yellow"></div>
+				<div className="light green"></div>
 			</div>
 		</div>
 	);
