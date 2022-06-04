cf101101af09ede3ae2cab85810306a99dfde8d9
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-04 01:29:51 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 //create your first component
 const TrafficLight = () => {
+	const [color, setColor] = useState("yellow");
 	return (
 		<div className="container">
 			<div className="lights">
-				<div className="light red glow"></div>
-				<div className="light yellow"></div>
-				<div className="light green"></div>
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"light red " + (color === "red" ? "glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"light yellow " + (color === "red" ? "glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"light green " + (color === "red" ? "glow" : "")
+					}></div>
 			</div>
 		</div>
 	);
